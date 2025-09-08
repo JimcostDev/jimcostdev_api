5e1d709e8a24cbbb33b27bd0648b84be5d673c60
JimcostDev <rjak727@gmail.com>
2025-09-08 13:33:26 +0200

    chore: Dockerfile completo, versión para prod

--- app/api/endpoints/healthcheck.py
+++ app/api/endpoints/healthcheck.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
 from core.config import settings
-from core.database import mongodb 
+from core.database import mongodb
 
 router = APIRouter()
 
-# Endpoint de verificación de salud 
 @router.get(
     "/healthcheck",
     include_in_schema=False,
@@ -13,19 +12,22 @@ router = APIRouter()
 )
 async def health_check():
     service_status = {
-        "status": "running",
+        "status": "healthy",
         "version": settings.PROJECT_VERSION,
         "dependencies": {
-            "database": "disconnected"
+            "database": "healthy"
         }
     }
-    
+
     # Verificación de la base de datos
     if mongodb.client:
         try:
-            await mongodb.client.admin.command('ping')
-            service_status["dependencies"]["database"] = "healthy"
+            await mongodb.client.admin.command("ping")
         except Exception as e:
             service_status["dependencies"]["database"] = f"unhealthy: {str(e)}"
-    
+            service_status["status"] = "degraded"
+    else:
+        service_status["dependencies"]["database"] = "disconnected"
+        service_status["status"] = "degraded"
+
     return service_status
